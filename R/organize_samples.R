7205f9b3dcb7cf67120839f43b377bfbc2bf4f84
zachpwakefield <zachpwakefield@gmail.com>
2024-02-01 15:05:05 -0500

    redo org samples

--- R/organize_samples.R
+++ R/organize_samples.R
@@ -3,30 +3,27 @@
 # # rmats
 # # hit
 
-organize_samples <- function(samples, cores) {
+organize_samples <- function(samples, cores = 1) {
   system(paste0("mkdir ", paste0(output_location, 'data/')))
-  sample_names <- unlist(parallel::mclapply(samples, mc.cores=cores, function(y) {
+  sample_names <- unlist(parallel::mclapply(ss, mc.cores = cores, function(y) {
     sample <- strsplit(y, '/')[[1]][length(strsplit(y, '/')[[1]])]
     system(paste0("mkdir ", paste0(output_location, 'data/', sample)))
     se_dir <- paste0(y, 'rmats/', "SE.MATS.JC.txt")
-    lapply(se_dir, function(x) {
-      system(
-        paste0("cp ", x, " ", output_location, 'data/', unlist(lapply(strsplit(x, '/'), "[[", 8)), '/',
-               unlist(lapply(strsplit(y, '/'), "[[", 8)), '.SEPSI')
-      )
-    })
+    system(
+      paste0("cp ", se_dir, " ", output_location, 'data/', sample, '/',
+                   sample, '.SEPSI')
+    )
     hit_dir <- paste0(y, 'hit/')
-    lapply(hit_dir, function(x) {
-      system(
-        paste0("cp ", x, list.files(x)[grep("exon", list.files(x))], " ", output_location, 'data/', unlist(lapply(strsplit(x, '/'), "[[", 8)), '/')
-      )
-      system(
-        paste0("cp ", x, list.files(x)[grep("ALE", list.files(x))], " ", output_location, 'data/', unlist(lapply(strsplit(x, '/'), "[[", 8)), '/')
-      )
-      system(
-        paste0("cp ", x, list.files(x)[grep("AFE", list.files(x))], " ", output_location, 'data/', unlist(lapply(strsplit(x, '/'), "[[", 8)), '/')
-      )
-    })
+    system(
+      paste0("cp ", hit_dir, list.files(hit_dir)[grep("exon", list.files(hit_dir))], " ", output_location, 'data/', sample, '/')
+    )
+    system(
+      paste0("cp ", hit_dir, list.files(hit_dir)[grep("ALE", list.files(hit_dir))], " ", output_location, 'data/', sample, '/')
+    )
+    system(
+      paste0("cp ", hit_dir, list.files(hit_dir)[grep("AFE", list.files(hit_dir))], " ", output_location, 'data/', sample, '/')
+    )
+
 
   }))
 }
