5b73181e5b6e1f462e9d49bddbd789cab2169a17
zachpwakefield <zachpwakefield@gmail.com>
2024-02-01 12:25:21 -0500

    fix pfam + parallelize

--- R/get_pfam.R
+++ R/get_pfam.R
@@ -1,4 +1,4 @@
-get_pfam <- function(background, foreground, pdir, output_location) {
+get_pfam <- function(background, foreground, pdir, output_location, cores) {
   pfam_hg38 <- read_tsv(paste0(pdir, '/protein_code_from_gencodev43_headerFix.txt.tsv'), col_names = F)
   pfam_hg38$transcriptID <- unlist(lapply(strsplit(pfam_hg38$X1, split = "#"),
                                           "[[", 1))
@@ -7,25 +7,31 @@ get_pfam <- function(background, foreground, pdir, output_location) {
                                                                     split = "#"), "[[", 2)), split = ";"), "[[", 1))
   pfam_hg38 <- pfam_hg38 %>% dplyr::relocate(geneID, .after = transcriptID)
 
-
-  fg_out <- do.call(rbind, mclapply(1:length(foreground$matched$tot_matched$input_id), mc.cores = 8, function(c) {
-    cx <- foreground$matched$tot_matched[c,]
-    cxdf <- pfam_hg38[pfam_hg38$transcriptID %in% cx$transcriptID,]
-    loc <- unlist(lapply(strsplit(cx$input_id, split = ";"), "[[", 2))
-    id <- unlist(lapply(strsplit(cxdf$X1, split = ";"), "[[", 1))
-    strand <- unlist(lapply(strsplit(cxdf$X1, split = ";"), "[[", 2))
-    cxdf$X1 <- paste0(id, ";", loc, ";", strand)
-    cxdf
+  fg_out <- do.call(rbind, parallel::mclapply(1:length(foreground$proBed$transcript), mc.cores = nCores, function(i) {
+    if (foreground$proBed$transcript[i] %in% pfam_hg38$transcriptID) {
+      df <- pfam_hg38[pfam_hg38$transcriptID == foreground$proBed$transcript[i],]
+      id <- paste0(foreground$proBed$transcript[i], "#",
+                   foreground$proBed$gene[i], ";",
+                   foreground$proBed$chr[i], ":",
+                   foreground$proBed$start[i], "-",
+                   foreground$proBed$stop[i], ";",
+                   foreground$proBed$strand[i])
+      df$X1 <- rep(id, length(df$X1))
+      df
+    }
   }))
-
-  bg_out <- do.call(rbind, mclapply(1:length(background$matched$tot_matched$input_id), mc.cores = 8, function(c) {
-    cx <- background$matched$tot_matched[c,]
-    cxdf <- pfam_hg38[pfam_hg38$transcriptID %in% cx$transcriptID,]
-    loc <- unlist(lapply(strsplit(cx$input_id, split = ";"), "[[", 2))
-    id <- unlist(lapply(strsplit(cxdf$X1, split = ";"), "[[", 1))
-    strand <- unlist(lapply(strsplit(cxdf$X1, split = ";"), "[[", 2))
-    cxdf$X1 <- paste0(id, ";", loc, ";", strand)
-    cxdf
+  bg_out <- do.call(rbind, paralell::mclapply(1:length(background$proBed$transcript), mc.cores = nCores, function(i) {
+    if (background$proBed$transcript[i] %in% pfam_hg38$transcriptID) {
+      df <- pfam_hg38[pfam_hg38$transcriptID == background$proBed$transcript[i],]
+      id <- paste0(background$proBed$transcript[i], "#",
+                   background$proBed$gene[i], ";",
+                   background$proBed$chr[i], ":",
+                   background$proBed$start[i], "-",
+                   background$proBed$stop[i], ";",
+                   background$proBed$strand[i])
+      df$X1 <- rep(id, length(df$X1))
+      df
+    }
   }))
 
   write_tsv(fg_out, paste0(output_location, "fgoutFast.fa.tsv"))
