70b60972b89bd6ea444a04a1ec170b7b6f1476d7
zachpwakefield <zachpwakefield@gmail.com>
2024-02-12 13:49:04 -0500

    updates for background transcript/bedify

---
+++ R/Jaccard.R
@@ -0,0 +1,9 @@
+# Function to calculate Jaccard index more efficiently
+calculate_jaccard <- function(start1, stop1, start2, stop2) {
+  intersection_length <- pmax(0, pmin(stop1, stop2) - pmax(start1, start2) + 1)
+  union_length <- (stop1 - start1 + 1) + (stop2 - start2 + 1) - intersection_length
+  jaccard_index <- intersection_length / union_length
+  length_jacc <- intersection_length/(abs(start2-stop2)+1)
+  return(list(jaccard_index = jaccard_index,
+              length_jacc = length_jacc))
+}
--- R/bedify.R
+++ R/bedify.R
@@ -35,34 +35,32 @@ bedifyForeground <- function(matched = matched, outname = outname, cores = 8) {
 }
 
 
-bedifyBackground <- function(matched = matched, outname = outname, cores) {
+bedifyBackground <- function(matched, outname, cores) {
 
-  # Extract necessary columns from the 'matched' dataframe
-  tID <- matched$transcriptID
-  eiID <- matched$input_id
-
-  toBed <- list() # Initialize an empty list to store BED format data
-
-  toBed <- parallel::mclapply(1:length(tID), mc.cores = cores, function(i) {
-    bed <- gtf[gtf$transcriptID == tID[i] & gtf$type == "exon",] %>% dplyr::select(chr, start, stop, transcriptID, geneID, strand)
-
-    # Add extra columns to the BED data: input ID, score (set to 0), constructed 'name' field combining transcriptID and input ID, delta.psi, and adjusted p-value
-    bed$eiID <- rep(eiID[i], length(gtf$geneID[gtf$transcriptID == tID[i] & gtf$type == "exon"]))
-    bed$score <- 0
-    bed$squish <- paste(bed$transcriptID, "#", bed$eiID, sep = "")
-
-    # Rename columns to match BED format specifications and select only necessary columns
-    colnames(bed) <- c('chrom','chromStart', 'chromEnd', 'transcriptID', "geneID", "strand", "eiID", "score", "name")
-    bed <- bed %>% dplyr::select(chrom, chromStart, chromEnd, name, score, strand)
+  # Function to process each transcriptID and construct BED rows
+  processTranscriptID <- function(i) {
+    # Efficiently filter for relevant rows
+    bed <- gtf[gtf$transcriptID == matched$transcriptID[i] & gtf$type == "exon", ]
+    bed <- bed %>%
+      dplyr::select(chr, start, stop, transcriptID, geneID, strand) %>%
+      dplyr::mutate(
+        eiID = matched$input_id[i],
+        score = 0,
+        name = paste(transcriptID, "#", matched$input_id[i], sep = "")
+      )
 
     # Adjust chromStart based on strand to adhere to BED format (0-based) conventions
-    if (unique(bed$strand) == "+") {
-      bed$chromStart <- as.integer(bed$chromStart) - 1
-    } else {
-      bed$chromStart <- as.integer(bed$chromEnd) + 1
-    }
-    bed # Return the modified BED data for this transcript
-  })
+    bed <- bed %>%
+      dplyr::mutate(chromStart = ifelse(strand == "+", as.integer(start) - 1, as.integer(stop) + 1)) %>%
+      dplyr::select(chrom = chr, chromStart, chromEnd = stop, name, score, strand)
+
+    return(bed)
+  }
+
+  toBed <- parallel::mclapply(1:nrow(matched), processTranscriptID, mc.cores = cores)
+
+  # Combine all BED rows into one dataframe
   toBed <- do.call(rbind, toBed)
-  return(toBed) # Return the final BED formatted data
+
+  return(toBed)
 }
