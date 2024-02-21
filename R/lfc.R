788437561242b7581bead59379b5571fb05c8149
zachpwakefield <zachpwakefield@gmail.com>
2024-02-21 16:41:36 -0500

    finally got rid of lfc metric

--- R/lfc.R
+++ R/lfc.R
@@ -1,61 +1,19 @@
-## This function computes the log fold change (LFC) for differential expression analysis between control and experimental groups.
-## It filters based on exon type, excludes outliers, and categorizes results based on significance and fold change direction.
-
-
-lfc <- function(de_df, numCont, numExp, exon_type, cores = 8, test_names, control_names) {
-  lfc <- list()  # Initialize list to store log fold changes
+diff_info <- function(de_df, numCont, numExp, exon_type, cores = 8, test_names, control_names) {
   col <- list()  # Initialize list to store colors for visualization
 
-  # Quick way to reference psi columns in forloop
-  searchers <- colnames(de_df)
-  searchers[grepl("_psi", colnames(de_df))] <- gsub("_psi", "", searchers[grepl("_psi", colnames(de_df))])
-
-  # Calculate log fold change for each exon
-  lfc <- mclapply(1:length(de_df$gene), mc.cores = cores, function(i) {
-    # Identify outliers in the dataset
-    outlier <- which(unlist(lapply(searchers, function(x) grepl(x, de_df$outlier[i]))))
-
-    # Define control and experimental groups, excluding outliers
-    cont <- which(grepl("psi", colnames(de_df)) & grepl(paste0(control_names, collapse = "|"), colnames(de_df)))
-    cont <- cont[!(cont %in% outlier)]
-    exp <- which(grepl("psi", colnames(de_df)) & grepl(paste0(test_names, collapse = "|"), colnames(de_df)))
-    exp <- exp[!(exp %in% outlier)]
-
-
-    # Compute log fold change based on available data, handling edge cases
-    if (length(exp) == 0) {
-      lfc_t <- log2((.01)/as.numeric(rowMeans(de_df[i,cont, drop = FALSE])+.01))
-    }
-    if (length(cont) == 0) {
-      lfc_t <- log2((as.numeric(rowMeans(de_df[i,exp, drop = FALSE]))+.01)/.01)
-    }
-    if (length(exp) == 0 & length(cont) == 0) {
-      lfc_t <- 0
-    }
-    if (length(exp) != 0 & length(cont) != 0) {
-      lfc_t <- log2((as.numeric(rowMeans(de_df[i,exp, drop = FALSE]))+.01)/as.numeric(rowMeans(de_df[i,cont, drop = FALSE])+.01))
-    }
-
-
-    lfc_t # Return the computed log fold change
-  })
-
-  # Assign computed log fold changes to the dataframe
-  de_df$lfc <- unlist(lfc)
   # Filter out non-significant and NA adjusted p-values
   de_df <- de_df[de_df$p.adj >= 0 & !is.na(de_df$p.adj),]
 
   # Assign colors based on log fold change and significance for visualization
   col <- lapply(1:length(de_df$gene), function(i) {
-    if (de_df$lfc[i] <= -1.0 & de_df$p.adj[i] < .05) {
+    if (de_df$delta.psi[i] <= -(2*thresh) & de_df$p.adj[i] < .05) {
       'brown'  # Color for significant negative log fold change
-    } else if (de_df$lfc[i] >= 1.0 & de_df$p.adj[i] < .05) {
+    } else if (de_df$delta.psi[i] >= (2*thresh) & de_df$p.adj[i] < .05) {
       'chartreuse4'  # Color for significant positive log fold change
     } else {
       "#A7A9AC"  # Default color for non-significant changes
     }
   })
-
 
 
   de_df$col <- unlist(col) # Add color information to the dataframe
--- R/make_lfcPlot.R
+++ R/make_lfcPlot.R
@@ -1,30 +1,30 @@
-make_lfcPlot <- function(lfc_df, pdir, num_thresh = 30) {
+make_dPsiPlot <- function(dpsi_df, pdir, num_thresh = 30) {
   # Load the gene reference conversion table
-  hg38.conv <- readRDS(paste0(pdir, "/hg38_geneRef_conv.RDS"))
+  hg38.conv <- readr::read_csv('/projectnb2/evolution/zwakefield/proteinImpacts/gencodev42_transcriptGeneProtein.csv')
 
   # Map Ensembl IDs in the lfc_df to HGNC symbols using the conversion table
 
   # Extract Ensembl ID before the dot
   # Return HGNC symbol if found
   # Return original Ensembl ID if HGNC symbol not found
-  lfc_df$hgnc <- unlist(lapply(lfc_df$gene, function(x) {ifelse(unlist(lapply(strsplit(x, split = "[.]"), "[[", 1)) %in% hg38.conv$ens,
-                                                                unique(hg38.conv$hgnc[hg38.conv$ens == unlist(lapply(strsplit(x, split = "[.]"), "[[", 1))]), x)
+  dpsi_df$hgnc <- unlist(lapply(dpsi_df$gene, function(x) {ifelse(unlist(lapply(strsplit(x, split = "[.]"), "[[", 1)) %in% hg38.conv$ens,
+                                                                  unique(hg38.conv$gene_name[hg38.conv$gene_id == unlist(lapply(strsplit(x, split = "[.]"), "[[", 1))]), x)
 
   }))
 
   # Prepare the data for labeling in the plot, sorting by absolute log fold change and adjusted p-value
-  lab_thresh <- lfc_df %>% dplyr::arrange(desc(abs(lfc)), p.adj)
+  lab_thresh <- dpsi_df %>% dplyr::arrange(desc(abs(delta.psi)), p.adj)
 
   # Remove gene labels for points below the threshold (either by p-value or log fold change)
-  lfc_df$hgnc[-log(lfc_df$p.adj) <= -log(lab_thresh$p.adj[num_thresh]) | abs(lfc_df$lfc) < abs(lab_thresh$lfc[num_thresh])]  <- ""
+  dpsi_df$hgnc[-log(dpsi_df$p.adj) <= -log(lab_thresh$p.adj[num_thresh]) | abs(dpsi_df$delta.psi) < abs(lab_thresh$delta.psi[num_thresh])]  <- ""
 
 
   # Create the log fold change plot using ggplot2
-  (deExons <- ggplot2::ggplot(lfc_df,ggplot2:: aes(x = lfc, y = -log(p.adj), color = col, label = hgnc)) + ggplot2::geom_point(ggplot2::aes(shape = type), size = 2, color = lfc_df$col) +
-      ggplot2::theme_classic() + ggplot2::ylab("-Log Adj P Value") + ggplot2::xlab("Log2FC")
+  (deExons <- ggplot2::ggplot(dpsi_df,ggplot2:: aes(x = delta.psi, y = -log(p.adj), color = col, label = hgnc)) + ggplot2::geom_point(ggplot2::aes(shape = type), size = 2, color = dpsi_df$col) +
+      ggplot2::theme_classic() + ggplot2::ylab("-Log2(FDR)") + ggplot2::xlab("Delta Psi")
     # +coord_cartesian(xlim = c(-100, 20))
     + ggplot2::geom_text(hjust=.2, vjust=0, size = 4)
   )
-  print("lfc plot done!") # Print completion message
+  print("delta psi plot done!") # Print completion message
   return(deExons) # Return the ggplot2 object for the plot
 }
