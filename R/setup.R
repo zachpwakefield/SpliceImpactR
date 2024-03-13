d501db76290995d9cac80d2f90801c0a8632682a
zachpwakefield <zachpwakefield@gmail.com>
2024-03-13 15:20:30 -0400

    CDS extract

--- R/setup.R
+++ R/setup.R
@@ -13,14 +13,21 @@ get_c_trans <- function(translations_location) {
 }
 
 get_c_nucs <- function(transcripts_location) {
-  nc <- readr::read_lines(transcripts_location)
+  nc <- readr::read_lines("/projectnb2/evolution/zwakefield/Annotations/hg38_gencode/gencode.v45.pc_transcripts.fa")
   transcript_title <- c(grep(">", nc), (length(nc)+1))
+  splitF <- strsplit(nc[(transcript_title[1:(length(transcript_title)-1)])], split = "[|]")
+  cdsL <- unlist(lapply(splitF, function(x) grep("CDS:", x)))
   tts <- gsub(">", "", unlist(lapply(strsplit(unlist(lapply(strsplit(nc[transcript_title[1:(length(transcript_title)-1)]], split = "[|]"), "[[", 1)), split = "[.]"), "[[", 1)))
+  cds_locs <- unlist(lapply(1:(length(transcript_title)-1), function(x) {
+    strsplit(unlist(lapply(strsplit(unlist(lapply(strsplit(nc[transcript_title[x]], split = "[|]"), "[[", cdsL[x])), split = ":"), "[[", 2)), split = "[-]")
+  }), recursive = F)
   c_trans <- unlist(lapply(1:(length(transcript_title)-1), function(x) {
-    c(tts[x], paste(nc[(transcript_title[x]+1):(transcript_title[x+1]-1)], collapse = ""))
+    c(tts[x], substr(paste(nc[(transcript_title[x]+1):(transcript_title[x+1]-1)], collapse = ""),
+                     as.numeric(cds_locs[[x]][1]), as.numeric(cds_locs[[x]][2]) ))
   }))
   transDF <- data.frame(transcriptID = c_trans[seq(1, length(c_trans), by = 2)],
                         code = c_trans[seq(2, length(c_trans), by = 2)])
+  # transDF$length <- nchar(transDF$code)
   return(list(c_trans = c_trans, transDF = transDF))
 }
 
