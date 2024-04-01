f1f6607ea72a64bdefcef7a072951d9f3d46107c
zachpwakefield <zachpwakefield@gmail.com>
2024-03-31 21:56:34 -0400

    reduce output

--- R/fullASoutcome.R
+++ R/fullASoutcome.R
@@ -5,7 +5,7 @@ fullASoutcome <- function(as_types = c("AFE", "ALE", "HFE", "HLE", "SE", "MXE", 
                           tti_location = "/projectnb/evolution/zwakefield/allison_mettl/analysis/sir/") {
   system(paste0("mkdir ",  output_directory))
   pdir <- system.file(package="SpliceImpactR")
-  fullOutcome <- lapply(as_types, function(x) {
+  lapply(as_types, function(x) {
     print(paste0(x, " analysis..."))
     system(paste0("mkdir ",  paste0(output_directory, x, "/")))
 
@@ -13,8 +13,6 @@ fullASoutcome <- function(as_types = c("AFE", "ALE", "HFE", "HLE", "SE", "MXE", 
                              test_group = test_group,control_group = control_group,
                              exon_type = x, cutoff = .2, cores = 6,
                              tti_location = tti_location)
-    fAS
   })
 
-  return(fullOutcome)
 }
