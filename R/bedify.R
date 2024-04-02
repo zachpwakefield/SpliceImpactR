bd275698cdfdc3518ec1ec76a9e7afacd413af60
zachpwakefield <zachpwakefield@gmail.com>
2024-04-02 14:44:40 -0400

    ddd

--- R/bedify.R
+++ R/bedify.R
@@ -2,13 +2,13 @@ bedifyForeground <- function(matched, outname, cores) {
 
   # Ensure gtf is filtered for "exon" type entries and select relevant columns upfront
   gtf_exons_limited <- gtf %>%
-    dplyr::filter(type == "exon") %>%
-    dplyr::select(start, stop, transcriptID)
+    dplyr::filter(.data$type == "exon") %>%
+    dplyr::select(.data$start, .data$stop, .data$transcriptID)
 
   # Creat a carbon copy of matched with changed column names to avoid ".x" and ".y"
   matched_carbon <- matched %>%
-    dplyr::rename(matchedStart = start) %>%
-    dplyr::rename(matchedStop = stop)
+    dplyr::rename(.data$matchedStart = start) %>%
+    dplyr::rename(.data$matchedStop = stop)
 
   # Left join matched data with gtf_exons based on transcriptID
   bed <- dplyr::left_join(matched_carbon, gtf_exons_limited, by = "transcriptID")
@@ -17,14 +17,14 @@ bedifyForeground <- function(matched, outname, cores) {
   bed <- bed %>%
     dplyr::mutate(
       score = 0,
-      name = paste(transcriptID, "#", input_id, sep = ""),
-      chromStart = ifelse(strand == "+", bed$start - 1, bed$start),
+      name = paste(.data$transcriptID, "#", .data$input_id, sep = ""),
+      chromStart = ifelse(.data$strand == "+", bed$start - 1, bed$start),
       chrom = bed$chr,
-      chromEnd =ifelse(strand == "+", bed$stop, bed$stop + 1)
+      chromEnd =ifelse(.data$strand == "+", bed$stop, bed$stop + 1)
     ) %>%
     dplyr::select(
-      chrom, chromStart, chromEnd, name, score, strand,
-      delta.psi, p.adj, add_inf = add_inf
+      .data$chrom, .data$chromStart, .data$chromEnd, .data$name, .data$score, .data$strand,
+      .data$delta.psi, .data$p.adj, add_inf = .data$add_inf
     )
 
   return(bed)
@@ -35,13 +35,13 @@ bedifyBackground <- function(matched, outname, cores) {
 
   # Ensure gtf is filtered for "exon" type entries and select relevant columns upfront
   gtf_exons_limited <- gtf %>%
-    dplyr::filter(type == "exon") %>%
-    dplyr::select(start, stop, transcriptID)
+    dplyr::filter(.data$type == "exon") %>%
+    dplyr::select(.data$start, .data$stop, .data$transcriptID)
 
   # Creat a carbon copy of matched with changed column names to avoid ".x" and ".y"
   matched_carbon <- matched %>%
-    dplyr::rename(matchedStart = start) %>%
-    dplyr::rename(matchedStop = stop)
+    dplyr::rename(matchedStart = .data$start) %>%
+    dplyr::rename(matchedStop = .data$stop)
 
   # Left join matched data with gtf_exons based on transcriptID
   bed <- dplyr::left_join(matched_carbon, gtf_exons_limited, by = "transcriptID")
@@ -50,13 +50,13 @@ bedifyBackground <- function(matched, outname, cores) {
   bed <- bed %>%
     dplyr::mutate(
       score = 0,
-      name = paste(transcriptID, "#", input_id, sep = ""),
-      chromStart = ifelse(strand == "+", bed$start - 1, bed$start),
+      name = paste(.data$transcriptID, "#", .data$input_id, sep = ""),
+      chromStart = ifelse(.data$strand == "+", bed$start - 1, bed$start),
       chrom = bed$chr,
-      chromEnd =ifelse(strand == "+", bed$stop, bed$stop + 1)
+      chromEnd =ifelse(.data$strand == "+", bed$stop, bed$stop + 1)
     ) %>%
     dplyr::select(
-      chrom, chromStart, chromEnd, name, score, strand
+      .data$chrom, .data$chromStart, .data$chromEnd, .data$name, .data$score, .data$strand
     )
 
   return(bed)
