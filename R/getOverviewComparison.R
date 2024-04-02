23faa8c0518bb36a5d5e2a71bd7e3972990d6a00
zachpwakefield <zachpwakefield@gmail.com>
2024-04-02 17:07:07 -0400

    goc init

---
+++ R/getOverviewComparison.R
@@ -0,0 +1,42 @@
+#' Get comparison between phenotypes: count of AS event, count of AS event per gene, ECDF compare
+#'
+#' @param data_list A list containing dataframes of each output file from HIT Index or rMATS as index 1 and "control" or "test" as index 2 for each position in the list
+#' @return 3 individual plots and 1 combined plot.
+#' @examples
+#' getOverviewComparison(load_output)
+getOverviewComparison <- function(data_list) {
+
+  ## Number of AS across phenotype
+  nASE <- lapply(data_list, function(x) nrow(x[x[,grep("PSI", colnames(x))] > 0,]))
+  dfCount <- data.frame(AScount = unlist(nASE),
+                        type = unlist(lapply(sample_types_sorted, "[[", 2)))
+  if (length(sample_types_sorted) > 8) {
+    p1 <- ggplot2::ggplot(dfCount, ggplot2::aes(x = type, y = AScount, fill = type)) + ggplot2::geom_dotplot(binaxis='y', stackdir='center') + ggplot2::theme_bw()+ggplot2::scale_fill_manual(values=c("brown", "chartreuse4"))+ ggplot2::xlab("Group") + ggplot2::ylab("Number of Events")
+  } else {
+    p1 <- ggplot2::ggplot(dfCount, ggplot2::aes(x = type, y = AScount, fill = type)) +
+      ggplot2::geom_dotplot(binaxis='y', stackdir='center') + ggplot2::theme_bw()+ggplot2::scale_fill_manual(values=c("brown", "chartreuse4"))+ ggplot2::xlab("Group") + ggplot2::ylab("Number of Events") +ggplot2::geom_violin(fill = NA)
+  }
+
+
+  ## Number of AS per gene across phenotype
+  nASpg <- lapply(data_list, function(x) mean(as.numeric(table(x$gene ))))
+  dfASpg <- data.frame(ASpg = unlist(nASpg),
+                       type = unlist(lapply(sample_types_sorted, "[[", 2)))
+  if (length(sample_types_sorted) > 8) {
+    p2 <- ggplot2::ggplot(dfASpg, ggplot2::aes(x = type, y = ASpg, fill = type)) + ggplot2::geom_dotplot(binaxis='y', stackdir='center') + ggplot2::theme_bw()+ggplot2::scale_fill_manual(values=c("brown", "chartreuse4")) + ggplot2::xlab("Group") + ggplot2::ylab("Mean events \n per gene")
+  } else {
+    p2 <- ggplot2::ggplot(dfASpg, ggplot2::aes(x = type, y = ASpg, fill = type)) + ggplot2::geom_dotplot(binaxis='y', stackdir='center') + ggplot2::theme_bw()+ggplot2::scale_fill_manual(values=c("brown", "chartreuse4")) + ggplot2::xlab("Group") + ggplot2::ylab("Mean events \n per gene") + ggplot2::geom_violin(fill = NA)
+  }
+  ## PSI distribution across phenotype
+  psiVals <- lapply(data_list, function(x) x[,grep("PSI", colnames(x))])
+  dfECDF <- data.frame(val = unlist(psiVals),
+                       type = unlist(lapply(1:length(psiVals), function(x) rep(unlist(lapply(sample_types_sorted, "[[", 2))[x], length(psiVals[[x]])))))
+
+  dfECDF <- dfECDF[dfECDF$val < 1 & dfECDF$val > 0,]
+  p3 <- ggplot2::ggplot(dfECDF, ggplot2::aes(x = val, colour = type, fill = type)) +
+    stat_ecdf(geom = "step") + ggplot2::theme_bw() + ggplot2::scale_color_manual(breaks=c("control","test"),
+                                                                                 values=c("brown", "chartreuse4")) + ggplot2::xlab("PSI") + ggplot2::ylab("ECDF")
+  comb_plot <- ggpubr::ggarrange(p1, p2, p3, labels = c("A", "B", "C"))
+
+  return(list(p1 = p1, p2 = p2, p3 = p3, comb_plot = comb_plot))
+}
