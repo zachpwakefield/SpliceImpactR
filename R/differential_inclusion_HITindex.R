2290b0a08f56611ed3a1eaf0a4cbccd64cd0c9ee
zachpwakefield <zachpwakefield@gmail.com>
2024-04-04 16:47:34 -0400

    fejnf

--- R/differential_inclusion_HITindex.R
+++ R/differential_inclusion_HITindex.R
@@ -5,7 +5,7 @@
 differential_inclusion_HITindex <- function(test_names, control_names, cores = 2, stat_model_bool = T, outlier_bool = T,
                                             outlier_threshold = c("4/n", "4/mean", "1", 1)[1], min_proportion_samples_per_phenotype = .2) {
   if ((length(test_names) + length(control_names)) < 6 & (stat_model_bool | outlier_bool) ) {
-    warning("Don't use the stat model and outlier filtering with low sample number (eg: <= 6)")
+    warning("The stat model and outlier filtering will not have much power with low sample number (eg: <= 6)")
   }
 
   sample_types <- list()
