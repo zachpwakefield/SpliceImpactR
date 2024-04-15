6dcd3167fc7a0794eb27de7ec9144fdde4d8422a
zachpwakefield <zachpwakefield@gmail.com>
2024-04-15 13:50:57 -0400

    update output location for bgPFAM

--- R/fullASoutcome.R
+++ R/fullASoutcome.R
@@ -22,7 +22,7 @@ fullASoutcome <- function(as_types = c("AFE", "ALE", "HFE", "HLE", "SE", "MXE", 
     fAS <- getfxnlASoutcome(output_location = paste0(output_directory, x, "/"),
                              test_group = test_group,control_group = control_group,
                              exon_type = x, cutoff = .2, outlier_handle = outlier_handle, cores = 6,
-                             tti_location = tti_location, full_pipe = T, mOverlap = mOverlap)
+                             tti_location = tti_location, full_pipe = T, mOverlap = mOverlap, bg = bg)
   })
 
 }
